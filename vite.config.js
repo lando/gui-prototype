e5e1b10989a2094ff4a7ca6051598679dddddcd7
John Ouellet <john@thinktandem.io>
2022-03-16 10:33:23 -0400

    Devops (#11)
    * Start of devops
    
    * Adjust vite config and other small changes

---
+++ vite.config.js
@@ -0,0 +1,48 @@
+import { defineConfig } from 'vite';
+import vuePlugin from '@vitejs/plugin-vue';
+import Path from 'path';
+import ViteFonts from 'vite-plugin-fonts';
+
+/**
+ * https://vitejs.dev/config
+ */
+const config = defineConfig({
+    root: Path.join(__dirname, 'src', 'renderer'),
+    publicDir: 'public',
+    server: {
+        port: 8080,
+    },
+    open: false,
+    build: {
+        outDir: Path.join(__dirname, 'build', 'renderer'),
+        emptyOutDir: true,
+    },
+    plugins: [
+        vuePlugin(),
+        ViteFonts({
+            google: {
+              families: [
+                {
+                  name: 'Lexend',
+                  styles: 'wght@400;700;800',
+                  defer: false
+                }
+              ]
+            },
+          })
+    ],
+    resolve: {
+        alias: {
+            '@': Path.join(__dirname, 'src', 'renderer')
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "./src/renderer/public/styles/index.scss";`
+            }
+        }
+    },
+});
+
+module.exports = config;
