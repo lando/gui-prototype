f5e077e4b00f25d8a2cdd5ba63fd3c5cc252af0e
John Ouellet <john@thinktandem.io>
2022-03-16 13:50:29 -0400

    Adjust tests (#16)
    * Adjust tests
    
    * Adjust linter

--- src/renderer/views/Updates.vue
+++ src/renderer/views/Updates.vue
@@ -1,58 +1,100 @@
 <template>
   <div class="plugins">
     <h1>Updates</h1>
-    <el-tabs v-model="activeName" class="plugin-tabs" @tab-click="handleClick">
-      <el-tab-pane label="Updates Available" name="first">
-        <div v-for="(plugin, index) in updatePlugins" :key="index" class="plugin needs-update">
-          <el-link class="plugin-name">{{ plugin.name }}</el-link>
-          <div class="plugin-version"> {{ plugin.installedVersion }} -> {{ plugin.newVersion }}</div>
-          <el-button :icon="Upload">Update</el-button>
-          <el-link class="plugin-relate-notes" :href="plugin.releaseNotes">Release Notes</el-link>
+    <el-tabs
+      v-model="activeName"
+      class="plugin-tabs"
+      @tab-click="handleClick"
+    >
+      <el-tab-pane
+        label="Updates Available"
+        name="first"
+      >
+        <div
+          v-for="(plugin, index) in updatePlugins"
+          :key="index"
+          class="plugin needs-update"
+        >
+          <el-link class="plugin-name">
+            {{ plugin.name }}
+          </el-link>
+          <div class="plugin-version">
+            {{ plugin.installedVersion }} -> {{ plugin.newVersion }}
+          </div>
+          <el-button :icon="Upload">
+            Update
+          </el-button>
+          <el-link
+            class="plugin-relate-notes"
+            :href="plugin.releaseNotes"
+          >
+            Release Notes
+          </el-link>
         </div>
-        <el-button type="primary">Update All</el-button>
+        <el-button type="primary">
+          Update All
+        </el-button>
       </el-tab-pane>
-      <el-tab-pane label="Updated Recently" name="second">
-        <div v-for="(plugin, index) in installedPlugins" :key="index" class="plugin installed">
-          <el-link :href="plugin.readme" class="plugin-name">{{ plugin.name }}</el-link>
-          <div class="plugin-version"> {{ plugin.installedVersion }}</div>
-          <el-link class="plugin-info" :href="plugin.readme">Docs</el-link>
-        </div>   
+      <el-tab-pane
+        label="Updated Recently"
+        name="second"
+      >
+        <div
+          v-for="(plugin, index) in installedPlugins"
+          :key="index"
+          class="plugin installed"
+        >
+          <el-link
+            :href="plugin.readme"
+            class="plugin-name"
+          >
+            {{ plugin.name }}
+          </el-link>
+          <div class="plugin-version">
+            {{ plugin.installedVersion }}
+          </div>
+          <el-link
+            class="plugin-info"
+            :href="plugin.readme"
+          >
+            Docs
+          </el-link>
+        </div>
       </el-tab-pane>
-    </el-tabs>   
+    </el-tabs>
   </div>
 </template>
 
 <script>
-import {defineComponent} from 'vue';
-import { Upload } from '@element-plus/icons-vue'
-
+// import {defineComponent} from 'vue';
+// import {Upload} from '@element-plus/icons-vue';
 
 export default {
   props: {
     updatePlugins: {
       type: Array,
       default: () => ([
-        {name: "Lando CLI", installedVersion: "v3.6.3", newVersion: "v3.6.4", releaseNotes: "someURL"},
-        {name: "PHP", installedVersion: "v0.5.2", newVersion: "v0.5.3", releaseNotes: "someURL"},
-        {name: "Drupal", installedVersion: "v0.5.1", newVersion: "v0.5.4", releaseNotes: "someURL"},
-        {name: "Laravel", installedVersion: "v0.5.0", newVersion: "v0.5.3", releaseNotes: "someURL"},
+        {name: 'Lando CLI', installedVersion: 'v3.6.3', newVersion: 'v3.6.4', releaseNotes: 'someURL'},
+        {name: 'PHP', installedVersion: 'v0.5.2', newVersion: 'v0.5.3', releaseNotes: 'someURL'},
+        {name: 'Drupal', installedVersion: 'v0.5.1', newVersion: 'v0.5.4', releaseNotes: 'someURL'},
+        {name: 'Laravel', installedVersion: 'v0.5.0', newVersion: 'v0.5.3', releaseNotes: 'someURL'},
       ]),
     },
     installedPlugins: {
       type: Array,
       default: () => ([
-        {name: "LAMP", installedVersion: "v0.5.2", readme: "someURL"},
-        {name: "Postgres", installedVersion: "v0.5.1", readme: "someURL"},
-        {name: "MariaDB", installedVersion: "v0.5.0",  readme: "someURL"},
-        {name: "WordPress", installedVersion: "v0.5.0", readme: "someURL"},
+        {name: 'LAMP', installedVersion: 'v0.5.2', readme: 'someURL'},
+        {name: 'Postgres', installedVersion: 'v0.5.1', readme: 'someURL'},
+        {name: 'MariaDB', installedVersion: 'v0.5.0', readme: 'someURL'},
+        {name: 'WordPress', installedVersion: 'v0.5.0', readme: 'someURL'},
       ]),
     },
     activeName: {
       type: String,
-      default: 'first'
-    }
-  }
-}
+      default: 'first',
+    },
+  },
+};
 
 </script>
 
@@ -78,7 +120,7 @@ export default {
     .plugin-name {
       width: 5rem;
     }
-   
+
     .plugin-info {
       justify-content: flex-end;
       flex-basis: 18rem;
