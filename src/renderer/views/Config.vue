24d1e216d4fe0db440324afcbca7b9182d4711fb
Alec Reynolds <alec+git@lando.dev>
2022-04-05 10:37:31 -0700

    Updates to error log prototype.

--- src/renderer/views/Config.vue
+++ src/renderer/views/Config.vue
@@ -24,14 +24,22 @@
         label="Logs"
         name="second"
       >
-        <el-input
-          v-model="errorLog"
-          :rows="10"
-          type="textarea"
-          disabled="true"
-        />
+        <el-row>
+          <el-input
+            v-model="errorLog"
+            :rows="10"
+            type="textarea"
+            disabled="true"
+          />
+        </el-row>
+        
+        <el-row>
+          <el-button @click="downloadLog">Download Log</el-button>
+        </el-row>
 
-        <el-button type="danger" @click="showFactoryReset = true">Factory Reset</el-button>
+        <el-row>
+          <el-button type="danger" @click="showFactoryReset = true">Factory Reset</el-button>
+        </el-row>
       </el-tab-pane>
     </el-tabs>
   </div>
@@ -64,9 +72,14 @@ const config = ref({
 
 const activeName = ref('first');
 
+// @todo: will want code highlighting, easy copying of values.
 const errorLog = ref('Stream of console output fed in here.');
 
 const showFactoryReset = ref(false);
+
+const downloadLog = () => {
+  //Logic to download the Lando log for debugging.
+};
 
 // const activeName = ref('first');
 </script>
@@ -80,5 +93,8 @@ const showFactoryReset = ref(false);
     label {
       width: 14rem;
     }
+  }
+  .el-row {
+    margin: 1rem 0;
   }
 </style>
