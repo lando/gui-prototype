9b885361a89a399f1d3fc6c672a0222753cd03e7
John Ouellet <john@thinktandem.io>
2022-05-09 07:41:29 -0400

    Update package.json + electron deeplinks

--- src/renderer/views/Callback.vue
+++ src/renderer/views/Callback.vue
@@ -9,10 +9,12 @@
 import {ref, onMounted} from 'vue';
 import {useRoute} from 'vue-router';
 const route = useRoute();
+const {auth} = window;
 
 const code = ref(null);
 if (route.query !== 'undefined' && route.query.code !== 'undefined') {
   code.value = route.query.code;
+  //await auth.loadTokens(code.value);
 }
 </script>
 
